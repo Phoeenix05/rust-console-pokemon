d4bd3cee95ea30ecf4eaab14f1ef6ca00f6b688b
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-15 21:50:30 +0300

    added pokemon selector

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,47 @@
 use inquire::Select;
 
-fn main() {
-    // let name = Text::new("What is yout name?").prompt();
+#[derive(Debug, Clone)]
+enum PokemonType {
+    Grass,
+    Fire,
+    Water,
+    Electric,
+}
 
-    // match name {
-    //     Ok(name) => println!("Hello, {}", name),
-    //     Err(_) => println!("An error happened when asking for your name, try again later."),
-    // }
-    
-    let options = vec!["Pikachu", "Charmander", "Bulbasaur", "Squirtle"];
-    
+struct Pokemon {
+    name: String,
+    pokemon_type: PokemonType,
+}
+
+macro_rules! pokemon {
+    ($name:expr, $pokemon_type:expr) => {
+        Pokemon {
+            name: $name,
+            pokemon_type: $pokemon_type,
+        }
+    };
+}
+
+// const POKEMON: Vec<Pokemon> = vec![
+//     pokemon!("Pikachu", PokemonType::Electric)
+// ];
+
+fn main() {
+    let pokemon: Vec<Pokemon> = vec![
+        pokemon!("Pikachu".to_string(), PokemonType::Electric),
+        pokemon!("Charmander".to_string(), PokemonType::Fire),
+        pokemon!("Bulbasaur".to_string(), PokemonType::Grass),
+        pokemon!("Squirtle".to_string(), PokemonType::Water),
+    ];
+
+    let options = pokemon
+        .iter()
+        .map(|pokemon| format!("{} ({:?})", pokemon.name.clone(), pokemon.pokemon_type.clone()))
+        .collect::<Vec<String>>();
+
     let answer = Select::new("Select a pokemon", options).prompt();
-    
+    // _ = Confirm::new("message").prompt();
+
     match answer {
         Ok(name) => println!("{}", name),
         Err(_) => println!("An error happened when asking you to select a pokemon"),
