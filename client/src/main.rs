ddd4a5cb2af6fc98de2517d6265098eddf98f0c9
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-16 14:20:40 +0300

    added serde to server dependencies

--- client/src/main.rs
+++ client/src/main.rs
@@ -12,18 +12,18 @@ use tui::{
 };
 
 fn ui<B: Backend>(f: &mut Frame<B>) {
+    let constraints = vec![
+        Constraint::Percentage(10),
+        Constraint::Percentage(80),
+        Constraint::Percentage(10),
+    ];
+
     let chunks = Layout::default()
         .direction(tui::layout::Direction::Vertical)
         .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10),
-            ]
-            .as_ref(),
-        )
+        .constraints(constraints.as_ref())
         .split(f.size());
+
     let block = Block::default().title("Block").borders(Borders::ALL);
     f.render_widget(block, chunks[0]);
     let block = Block::default().title("Block 2").borders(Borders::ALL);
@@ -36,13 +36,7 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        // let size = f.size();
-        // let block = Block::default().title("Block").borders(Borders::ALL);
-        // f.render_widget(block, size);
-        ui(f);
-    })?;
-    // ui(terminal);
+    terminal.draw(|f| ui(f))?;
 
     thread::sleep(Duration::from_millis(5000));
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,29 @@
-// use figment::{Figment, providers::Toml};
+// use std::sync::Mutex;
+
+// use serde::Serialize;
 
 #[macro_use]
 extern crate rocket;
 
-// use rocket::figment::providers::Toml;
+// static SERVERS: Mutex<Vec<GameServer>> = Mutex::new(vec![]);
+
+// #[derive(Serialize)]
+// struct GameServer {
+//     name: String,
+//     id: String,
+// }
+
+// #[get("/servers")]
+// fn get_servers() -> Vec<String> {
+//     let servers = SERVERS.lock().unwrap();
+//     servers
+//         .into_iter()
+//         .map(|server| server.name)
+//         .collect::<Vec<String>>()
+//         .clone()
+// }
 
 #[launch]
 fn rocket() -> _ {
-    // let figment = rocket::Config::figment().merge(Toml::)
-    // let figment = Figment::from(rocket::Config::default()).merge(Toml::)
-
     rocket::build().mount("/", routes![])
 }
